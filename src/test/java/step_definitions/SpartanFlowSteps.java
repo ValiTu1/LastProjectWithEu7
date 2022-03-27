3e7002cd90827b1b02234dd56703dbd90ad44c07
Vali Tugui <vali.tuguii@gmail.com>
2022-03-27 14:53:52 +0100

    Initial commit

---
+++ src/test/java/step_definitions/SpartanFlowSteps.java
@@ -0,0 +1,11 @@
+package step_definitions;
+
+import io.cucumber.java.en.When;
+
+public class SpartanFlowSteps {
+
+    @When("Users send a request to Spartan API id {int}")
+    public void users_send_a_request_to_spartan_api_id(int id) {
+
+    }
+}
---
+++ src/test/resources/features/spartanFlow.feature
@@ -0,0 +1,5 @@
+Feature: Test Spartan API with complete CRUD operations
+
+  @wip
+  Scenario: Read data from Spartan API
+    When Users send a request to Spartan API id 7
