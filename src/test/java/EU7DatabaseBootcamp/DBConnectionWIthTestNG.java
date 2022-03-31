79c02043e1889123c3b8749fe96bc73ba39879e3
Vali Tugui <vali.tuguii@gmail.com>
2022-03-31 18:46:29 +0100

    DB utils added

--- src/test/java/EU7DatabaseBootcamp/DBConnectionWIthTestNG.java
+++ src/test/java/EU7DatabaseBootcamp/DBConnectionWIthTestNG.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DBConnectionWIthTestNG {
 
@@ -13,7 +15,7 @@ public class DBConnectionWIthTestNG {
     Statement statement;
     ResultSet resultSet;
 
-    String query = "select name from spartans";
+    String query = "select name, gender from spartans where name = 'Oscar'";
     @BeforeMethod
     public void connectToDB() throws SQLException {
         connection = DriverManager.getConnection(ConfigurationReader.get("spartan.DBUrl"),
@@ -30,7 +32,28 @@ public class DBConnectionWIthTestNG {
     }
 
     @Test
-    public void Test1(){
+    public void Test1() throws SQLException {
+        while(resultSet.next()){
+            //System.out.println(resultSet.getString(1) + " | " + resultSet.getString(2));
+        }
+    }
+
+    @Test
+    public void test2() throws SQLException {
+        ResultSetMetaData rsmd = resultSet.getMetaData(); // this object gives us column info
+        int columnCount = rsmd.getColumnCount();
+
+        Map<String, Object> row = new HashMap<>();
+
+        while(resultSet.next()){
+            String result = "";
+            for (int i =1; i<= columnCount; i++) {
+                row.put(rsmd.getColumnName(i), resultSet.getObject(i));
+                result+= " " + resultSet.getString(i);
+            }
+            System.out.println(row);
+            System.out.println(result);
+        }
 
     }
 
